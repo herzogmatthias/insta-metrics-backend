57f14643abda777d0fc050ab3023494e181a26e1
MatthiasHerzog2000 <gfmerwin@gmail.com>
2020-04-25 18:26:10 +0200

    fixing login error

--- src/api/LoginController.ts
+++ src/api/LoginController.ts
@@ -3,7 +3,7 @@ let jwt = require("jsonwebtoken");
 
 export const login = (req: e.Request, res: e.Response) => {
   const pw = req.body.password;
-  if (pw === process.env.secret) {
+  if (pw === process.env.password) {
     const token = jwt.sign({ data: pw }, process.env.secret, {
       expiresIn: 604800, //1 week
     });
